5484d8c17dfa931d3b487e641682be531152d524
dong <dev.dong9897@gmail.com>
2023-05-26 17:34:25 +0900

    feat: visual.js 설명문 추가

--- js/visula.js
+++ js/visula.js
@@ -1,28 +1,34 @@
-window.addEventListener("load", function () {
-    
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener("readystatechange", function (event) {
-    let req = event.target;
-    if (req.readyState === XMLHttpRequest.DONE) {
-      let data = JSON.parse(req.response);
-      makeVisualHtml(data);
-    }
-  });
-  xhr.open("GET", "dada/visualdata.json");
-  xhr.send();
-
-  function makeVisualHtml(_data) {
-    let html = ``;
-    _data.img.forEach((item) => {
-      let temp = `
-                <div class="swiper-slide" style="background-image: url(images/${item})"></div>
-            `;
-      html += temp;
+/**
+ * 작성자 : 남동욱
+ * 작성일 : 2023-05-23
+ * 기능 업데이트 : json 데이터를 이용 
+ */
+window.addEventListener("load", function (event) {
+    const swVisualWrap = document.querySelector(".sw-visual .swiper-wrapper");
+  
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function (event) {
+      let req = event.target;
+      if (req.readyState === XMLHttpRequest.DONE) {
+        let data = JSON.parse(req.response);
+        makeVisualHtml(data);
+      }
     });
-
-    swVisualWrap.innerHTML = html;
-
-    const awVisual = new Swiper(".sw-visual", {
+    xhr.open("GET", "data/visualdata.json");
+    xhr.send();
+  
+    function makeVisualHtml(_data) {
+      let html = ``;
+      _data.img.forEach((item) => {
+        let temp = `
+              <div class="swiper-slide" style="background-image: url(images/${item})"></div>
+          `;
+        html += temp;
+      });
+  
+      swVisualWrap.innerHTML = html;
+  
+      const swVisual = new Swiper(".sw-visual", {
         loop: true,
         effect: "fade",
         speed: 800,
@@ -31,9 +37,5 @@ window.addEventListener("load", function () {
           disableOnInteraction: false,
         },
       });
-
-  }
-
-  
-  
-});
+    }
+  });
