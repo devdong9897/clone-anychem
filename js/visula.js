c1b0369ce95eeab723daf02dd20ae1c914cf8c19
dong <dev.dong9897@gmail.com>
2023-05-30 18:11:02 +0900

    feat: visual 작업중...

--- js/visula.js
+++ js/visula.js
@@ -1,41 +1,40 @@
 /**
  * 작성자 : 남동욱
- * 작성일 : 2023-05-23
- * 기능 업데이트 : json 데이터를 이용 
+ * 작성일 : 2023-05-26
+ * 기능 업데이트 : json 데이터를 이용한 html 구조 생성 적용
  */
 window.addEventListener("load", function (event) {
-    const swVisualWrap = document.querySelector(".sw-visual .swiper-wrapper");
-  
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener("readystatechange", function (event) {
-      let req = event.target;
-      if (req.readyState === XMLHttpRequest.DONE) {
-        let data = JSON.parse(req.response);
-        makeVisualHtml(data);
-      }
+  const swVisualWrap = document.querySelector(".sw-visual .swiper-wrapper");
+
+  fetch("data/visualdata.json")
+    .then((res) => res.json())
+    .then((result) => makeVisualHtml(result))
+    .catch((err) => console.log(err));
+
+  function makeVisualHtml(_data) {
+    let html = ``;
+    _data.img.forEach((item) => {
+      let temp = `
+            <div class="swiper-slide" style="background-image: url(images/${item})"></div>
+        `;
+      html += temp;
     });
-    xhr.open("GET", "data/visualdata.json");
-    xhr.send();
-  
-    function makeVisualHtml(_data) {
-      let html = ``;
-      _data.img.forEach((item) => {
-        let temp = `
-              <div class="swiper-slide" style="background-image: url(images/${item})"></div>
-          `;
-        html += temp;
-      });
-  
-      swVisualWrap.innerHTML = html;
-  
-      const swVisual = new Swiper(".sw-visual", {
-        loop: true,
-        effect: "fade",
-        speed: 800,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-      });
-    }
-  });
+
+    swVisualWrap.innerHTML = html;
+
+    const swVisual = new Swiper(".sw-visual", {
+      loop: true,
+      effect: "fade",
+      speed: 800,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".sw-visual-pg",
+        clickable: true,
+        
+      },
+    });
+  }
+});
